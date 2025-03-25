57998bdef8b89f93f23535d0232d0c2ee3507167
hatchikuji <swannbrillant@gmail.com>
2025-03-25 15:16:51 +0100

    initialize Program class and create instances of Passager and Voiture

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 
-Console.WriteLine("Hello, World!");
+namespace gittest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Passager passager1 = new Passager("Dupont", "Jean", 25);
+            Voiture voiture1 = new Voiture("Renault", "Clio", "Bleu", 2010, 50);
+            Voiture voiture2 = new Voiture("Peugeot", "208", "Rouge", 2015, 60);
+            Voiture voiture3 = new Voiture("Citroen", "C3", "Vert", 2018, 70);
+
+        }
+    }
+}
