e723e094806e68a39494b61b08d5ca38af93a20d
hatchikuji <swannbrillant@gmail.com>
2025-03-25 14:45:54 +0100

    add Passager class with properties and constructor

---
+++ Passager.cs
@@ -0,0 +1,18 @@
+﻿namespace gittest;
+
+public class Passager
+{
+    private string _nom;
+    private string _prenom;
+    private int _age;
+    private List<Voiture> _voiture;
+
+    public Passager(string nom, string prenom, int age)
+    {
+        _nom = nom;
+        _prenom = prenom;
+        _age = age;
+        _voiture = new List<Voiture>();
+    }
+
+}
