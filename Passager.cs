79534020c4e034944a51b7734c6d2b44f91465b5
hatchikuji <swannbrillant@gmail.com>
2025-03-25 14:52:03 +0100

    add properties and methods to Passager class for managing Voiture instances

--- Passager.cs
+++ Passager.cs
@@ -15,4 +15,41 @@ public class Passager
         _voiture = new List<Voiture>();
     }
 
+    public string Nom
+    {
+        get { return _nom; }
+    }
+
+    public string Prenom
+    {
+        get { return _prenom; }
+    }
+
+    public int Age
+    {
+        get { return _age; }
+    }
+
+    public int NbVoiture()
+    {
+        return _voiture.Count;
+    }
+
+    public void AjouterVoiture(Voiture voiture)
+    {
+        _voiture.Add(voiture);
+    }
+
+    public void SupprimerVoiture(Voiture voiture)
+    {
+        _voiture.Remove(voiture);
+    }
+
+    public void AfficherVoiture()
+    {
+        foreach (Voiture voiture in _voiture)
+        {
+            Console.WriteLine(voiture.marque + " " + voiture.modele + " " + " " + voiture.couleur + " " + voiture.annee);
+        }
+    }
 }
