fadca7dc4c98dbbc0595db00e0e87d28a66997c9
Kaldéo <gabin.freval@gmail.com>
2025-03-25 15:21:23 +0100

    new passagé

--- Program.cs
+++ Program.cs
@@ -12,6 +12,10 @@ namespace gittest
             Voiture voiture2 = new Voiture("Peugeot", "208", "Rouge", 2015, 60);
             Voiture voiture3 = new Voiture("Citroen", "C3", "Vert", 2018, 70);
 
+            passager1.AjouterVoiture(voiture1);
+            passager1.AjouterVoiture(voiture2);
+            passager1.AjouterVoiture(voiture3);
+            passager1.AfficherVoiture();
         }
     }
 }
